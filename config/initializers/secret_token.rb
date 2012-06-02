d67cc52172b37a1350ce9c2ef4f220eee5fc7421
Timothy Ahong <timothy.ahong@eventmobi.com>
2012-06-02 10:18:14 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GTasksContract::Application.config.secret_token = 'd3b85e5812fe75d7fd8790a081cec17285ca5ff2a3f5ec25d52bfd4ca52e2578b3f886c4eb0048e89b94d4656d4ab8f4c4574fa1adfe5113bbc5de5d59190be7'
