391e913d6dba093a836fc7a8f71cf2131fc973fa
Timothy Ahong <timothy.ahong@eventmobi.com>
2012-06-08 19:14:57 -0400

    a

--- app/assets/javascripts/DOMUpdate.js
+++ app/assets/javascripts/DOMUpdate.js
@@ -1,11 +1,30 @@
 function updateDOM(){
+	//clear tasks
+	clearTasks()
 	//for every task 
 	for(x in tasks){
-		//check if has date
-		//check if important
+		hasDate = (tasks[x].due===undefined)
+		important = (tasks[x].listname=='Important')
+		if(hasDate){
+			if(important){var selector = 'tr';}
+			else {var selector = 'br';}
+		}
+		else{
+			if(important){var selector = 'tl';}
+			else {var selector = 'bl';}
+		}
+		addTask(tasks[x],selector)
 	}
 }
 
-function addTask(task,hasDate,important){
-	
+function addTask(task,selector){
+	var prev = $('.'+selector).find('#inner').html()
+	prev+= "<div>"+task.title+"</div>"
+}
+
+function clearTasks(){
+	$('.tl').html("")
+	$('.tr').html("")
+	$('.bl').html("")
+	$('.br').html("")
 }
