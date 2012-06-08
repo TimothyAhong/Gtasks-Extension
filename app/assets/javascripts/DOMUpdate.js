fd1eb51dd84358588bf6d84e6d1413f828e32515
Timothy Ahong <timothy.ahong@eventmobi.com>
2012-06-08 18:59:44 -0400

    a

--- app/assets/javascripts/DOMUpdate.js
+++ app/assets/javascripts/DOMUpdate.js
@@ -1,4 +1,11 @@
 function updateDOM(){
-	//access local storage
-	//update the lists
+	//for every task 
+	for(x in tasks){
+		//check if has date
+		//check if important
+	}
+}
+
+function addTask(task,hasDate,important){
+	
 }
